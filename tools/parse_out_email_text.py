16e30ab499a8a78eefacb5be56b5650823a7fee4
Murray Lynch <mdlynch37@gmail.com>
2016-06-28 11:37:44 -0400

    Files up to stemming quiz in text_learning (this and last commit not done properly, but up-to-date.

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -3,17 +3,17 @@
 from nltk.stem.snowball import SnowballStemmer
 import string
 
-def parseOutText(f):
+def parseOutText(f, stemmed=True):
     """ given an opened email file f, parse out all text below the
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -24,31 +24,33 @@ def parseOutText(f):
     content = all_text.split("X-FileName:")
     words = ""
     if len(content) > 1:
+        # for use in interactive shell
+        global text_string
         ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
-
+        text_string = content[1].translate(string.maketrans("", ""),
+                                           string.punctuation)
         ### project part 2: comment out the line below
-        words = text_string
-
-        ### split the text string into individual words, stem each word,
-        ### and append the stemmed word to words (make sure there's a single
-        ### space between each stemmed word)
-        
-
-
-
+        # my way: control flow with boolean argument to parseOutText()
+        if not stemmed:
+            words = text_string
+        else:
+            ### split the text string into individual words, stem each word,
+            ### and append the stemmed word to words (make sure there's a
+            ### single space between each stemmed word)
+            stemmer = SnowballStemmer('english')
+            stemmed_words = [stemmer.stem(w) for w in text_string.split()]
+            words = ' '.join(stemmed_words)
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
     text = parseOutText(ff)
-    print text
+    print (text)
 
 
 
 if __name__ == '__main__':
     main()
-
