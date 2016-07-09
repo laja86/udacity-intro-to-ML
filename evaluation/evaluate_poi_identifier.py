9c5483e6b614c341a4e511347e61b28895bc7573
Murray Lynch <mdlynch37@gmail.com>
2016-07-09 10:26:53 -0400

    End of Intro to Machine Learning, still needs some organization perhaps.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,14 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+accuracy_score(pred, labels_test)
 
